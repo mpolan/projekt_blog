2a46227b51fac8874619959ee29d9dc50ee72607
Michał Polanowski <m.polanowski0@gmail.com>
2025-05-13 01:24:56 +0200

    Zapobieganie przechowywania SECRET_KEY w settings.py

--- personal_blog/personal_blog/settings.py
+++ personal_blog/personal_blog/settings.py
@@ -18,9 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qxo%pg=$sif9-14@37-w(*mb0f5j(q44*3xad4+y(5#q=al)98'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
