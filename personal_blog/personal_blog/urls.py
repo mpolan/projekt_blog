25e01f077f788b81309d55bfdf935bd24dc4b444
Michał Polanowski <m.polanowski0@gmail.com>
2025-05-13 01:43:54 +0200

    test poprawienia braku wyswietlania awatarow uzytkownikow

--- personal_blog/personal_blog/urls.py
+++ personal_blog/personal_blog/urls.py
@@ -31,8 +31,7 @@ urlpatterns = [
     #path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
 
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
