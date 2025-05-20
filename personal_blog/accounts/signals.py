b82f996b96a6e41f2be5c19bbe95c32ddb884b54
Krzysiek <krzyssemeniuk@gmail.com>
2025-05-20 12:06:19 +0200

    dodanie przywilejow dla moderatora, zrobienie strony dla moderatorow

--- personal_blog/accounts/signals.py
+++ personal_blog/accounts/signals.py
@@ -2,7 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
-
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 # @receiver(post_save, sender=User)
 # def create_or_update_user_profile(sender, instance, created, **kwargs):
 #     if created:
@@ -13,3 +14,12 @@ from .models import Profile
 #             instance.profile.save()
 #         except Profile.DoesNotExist:
 #             Profile.objects.create(user=instance)
+
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def add_default_group(sender, instance, created, **kwargs):
+    if created:
+        user_group = Group.objects.get(name='Użytkownik')
+        instance.groups.add(user_group)
--- personal_blog/blog/models.py
+++ personal_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 VISIBILITY_CHOICES = (
     ('public', 'Publiczny'),
     ('private', 'Tylko dla mnie'),
