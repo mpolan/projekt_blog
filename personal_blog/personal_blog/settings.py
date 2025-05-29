b3286af29fe17266905f2fb3f1bf5d0967beb83c
Michał Polanowski <m.polanowski0@gmail.com>
2025-05-29 21:00:38 +0200

    Update settings.py

--- personal_blog/personal_blog/settings.py
+++ personal_blog/personal_blog/settings.py
@@ -41,8 +41,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'personal_blog.urls'
 
 TEMPLATES = [
